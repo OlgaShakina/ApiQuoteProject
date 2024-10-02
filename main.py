8cb2533937ce2446420f4b90c6d43d84f12e6af8
OlgaShakina <olga.buinova@gmail.com>
2024-10-02 15:43:44 +0300

    ad. new code

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+
+app = Flask(__name__)
+
+def get_random_quote():
+    response = requests.get("https://api.quotable.io/random")
+    if response.status_code == 200:
+        return response.json()
+    return None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    quote = None
+    if request.method == 'POST':
+        quote = get_random_quote()
+    return render_template('index.html', quote=quote)
+
+if __name__ == '__main__':
+    app.run(debug=True)
